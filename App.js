794b686c7769429a4a7fbe7a07061b6d01272a21
Pranoy Sarkar <pranoy0211@gmail.com>
2022-03-14 21:46:04 +1000

    init push
    app asks for your location

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+  useEffect(() => {
+    load();
+  });
+  async function load() {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access location was denied");
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      const { longitude, latitude } = location.coords;
+      alert(`You are at ${latitude}, ${longitude}`);
+    } catch (error) {
+      alert(error);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello People</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0a0a0a",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
   },
 });
